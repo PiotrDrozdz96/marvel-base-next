b13a3b1a3805ad70d527b27f04091a64ff95d2a2
Piotr Drożdż <drozdz.piotr96@gmail.com>
2022-02-19 23:16:51 +0100

    feat: messages

---
+++ src/@components/Home/Home.messages.ts
@@ -0,0 +1,4 @@
+export default {
+  welcomeTo: 'Witamy w',
+  projectName: 'Marvel Base',
+};
--- src/@components/Home/Home.tsx
+++ src/@components/Home/Home.tsx
@@ -1,9 +1,10 @@
 import classes from './Home.module.scss';
+import messages from './Home.messages';
 
 const Home = (): JSX.Element => (
   <main className={classes.main}>
     <h1 className={classes.title}>
-      Witamy w <span className={classes.name}>Marvel Base</span>
+      {messages.welcomeTo} <span className={classes.name}>{messages.projectName}</span>
     </h1>
   </main>
 );
