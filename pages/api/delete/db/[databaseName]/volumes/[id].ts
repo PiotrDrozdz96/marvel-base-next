a03901df4bf465c48ff4829d4ab503a4f403c73a
Piotr Drożdż <drozdz.piotr96@gmail.com>
2022-02-24 22:37:36 +0100

    feat: volumes api crud

---
+++ pages/api/delete/db/[databaseName]/volumes/[id].ts
@@ -0,0 +1,49 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import fs from 'fs';
+
+import JsonData from 'types/JsonData';
+import { ApiVolume } from 'types/Volume';
+import messages from 'utils/apiValidators/apiValidators.messages';
+import { interpolate } from 'utils/interpolate';
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) =>
+  new Promise((resolve) => {
+    if (req.method !== 'DELETE') {
+      resolve(res.status(405).send({ message: messages.delete }));
+      return;
+    }
+
+    const id = Number(req.query.id);
+    const { databaseName } = req.query as Record<string, string>;
+
+    fs.readFile(`src/database/db/${databaseName}/volumes.json`, 'utf8', (err, data) => {
+      if (err) {
+        resolve(res.status(404).json(err));
+        return;
+      }
+
+      const { volumes: database, meta } = JSON.parse(data) as JsonData<'volumes', ApiVolume>;
+      const result = database[id];
+      if (!result) {
+        resolve(res.status(404).json({ message: interpolate(messages.notFound, { id, baseName: 'volumes' }) }));
+        return;
+      }
+
+      delete database[id];
+
+      const newDatabase = {
+        volumes: database,
+        meta,
+      };
+
+      fs.writeFile(`src/database/db/${databaseName}/volumes.json`, JSON.stringify(newDatabase, null, 2), (writeErr) => {
+        if (err) {
+          resolve(res.status(500).send(writeErr));
+          return;
+        }
+        resolve(res.status(200).json({ ...result, id }));
+      });
+    });
+  });
+
+export default handler;
