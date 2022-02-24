1602cb449bb65784b19a596e749fc8ed9ec60f82
Piotr Drożdż <drozdz.piotr96@gmail.com>
2022-02-24 19:11:11 +0100

    feat: api delete series

--- pages/api/delete/db/[databaseName]/series/[id].ts
+++ pages/api/delete/db/[databaseName]/series/[id].ts
@@ -2,7 +2,7 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import fs from 'fs';
 
 import JsonData from 'types/JsonData';
-import { ApiWave } from 'types/Wave';
+import { ApiSerie } from 'types/Serie';
 import messages from 'utils/apiValidators/apiValidators.messages';
 import { interpolate } from 'utils/interpolate';
 
@@ -16,27 +16,27 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) =>
     const id = Number(req.query.id);
     const { databaseName } = req.query as Record<string, string>;
 
-    fs.readFile(`src/database/db/${databaseName}/waves.json`, 'utf8', (err, data) => {
+    fs.readFile(`src/database/db/${databaseName}/series.json`, 'utf8', (err, data) => {
       if (err) {
         resolve(res.status(404).json(err));
         return;
       }
 
-      const { waves: database, meta } = JSON.parse(data) as JsonData<'waves', ApiWave>;
+      const { series: database, meta } = JSON.parse(data) as JsonData<'series', ApiSerie>;
       const result = database[id];
       if (!result) {
-        resolve(res.status(404).json({ message: interpolate(messages.notFound, { id, baseName: 'waves' }) }));
+        resolve(res.status(404).json({ message: interpolate(messages.notFound, { id, baseName: 'series' }) }));
         return;
       }
 
       delete database[id];
 
       const newDatabase = {
-        waves: database,
+        series: database,
         meta,
       };
 
-      fs.writeFile(`src/database/db/${databaseName}/waves.json`, JSON.stringify(newDatabase, null, 2), (writeErr) => {
+      fs.writeFile(`src/database/db/${databaseName}/series.json`, JSON.stringify(newDatabase, null, 2), (writeErr) => {
         if (err) {
           resolve(res.status(500).send(writeErr));
           return;
