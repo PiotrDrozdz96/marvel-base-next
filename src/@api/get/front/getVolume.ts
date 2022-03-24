173fe5080bfc25976bd74f3d00c8ea4b33d77367
Piotr Drożdż <drozdz.piotr96@gmail.com>
2022-03-24 19:05:58 +0100

    fix: get volume

--- src/@api/get/front/getVolume.ts
+++ src/@api/get/front/getVolume.ts
@@ -9,12 +9,6 @@ const getVolume = async (databaseName: string, id: number): Promise<FrontVolume 
     volume.id = id;
     volume.serieName = volume.serie_id ? (series[volume.serie_id].name as string) : '';
 
-    if (volume.notebooks_ids?.length) {
-      const { notebooks } = await get(databaseName, 'notebooks');
-      volume.notebooks = volume.notebooks_ids.map((notebookId) => ({ ...notebooks[notebookId], id: notebookId }));
-    } else {
-      volume.notebooks = [];
-    }
     return volume as FrontVolume;
   } catch {
     return undefined;
