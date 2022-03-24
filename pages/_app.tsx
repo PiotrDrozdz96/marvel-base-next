af7177f935a7dc8f9878564c7033f1f430ad96ca
Piotr Drożdż <drozdz.piotr96@gmail.com>
2022-03-24 21:01:46 +0100

    feat: add daredevil

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,5 @@
-/* eslint-disable react/no-unescaped-entities */
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import Script from 'next/script';
 
 import Navbar from '@components/Navbar';
 
@@ -14,7 +12,6 @@ const MyApp = ({ Component, pageProps }: AppProps) => (
       <meta name="description" content={`Marvel Base ${pageProps.title || ''}`} />
       <link rel="icon" href="/favicon.ico" />
     </Head>
-    <Script type="text/javascript">window['__react-beautiful-dnd-disable-dev-warnings'] = true;</Script>
     {pageProps.menu && <Navbar menu={pageProps.menu} />}
     <Component {...pageProps} />
   </>
