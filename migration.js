0b2f10e512a67db9b2a1b8efcd3bda576fa0c60e
Piotr Drożdż <drozdz.piotr96@gmail.com>
2022-07-17 17:05:38 +0200

    feat: migration volumes

---
+++ migration.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+
+const databasename = 'marvel-now-2.0';
+
+fs.readFile(`./src/database/db/${databasename}/volumes.json`, 'utf8', (_, volumesData) => {
+  fs.readFile(`./src/database/db/${databasename}/notebooks.json`, 'utf8', (__, notebooksData) => {
+    const result = {};
+    const { volumes, meta } = JSON.parse(volumesData);
+    const { notebooks } = JSON.parse(notebooksData);
+    const volumesKeys = Object.keys(volumes);
+
+    volumesKeys.forEach((key) => {
+      const { notebooks_ids: notebooksIds, ...volume } = volumes[key];
+      const notebooksUrls = notebooksIds.map((id) => {
+        const notebook = notebooks[id];
+        return `${notebook.title} ${notebook.vol} ${notebook.no}`.replaceAll(' ', '_');
+      });
+      result[key] = { ...volume, notebooks: notebooksUrls };
+    });
+
+    const resultString = JSON.stringify({ volumes: result, meta }, null, 2);
+
+    fs.writeFile(
+      `./src/database/db/${databasename}/volumes-m.json`,
+      resultString.replace(/\s+(?=[^[\]]*\])/gm, ''),
+      () => {}
+    );
+  });
+});
